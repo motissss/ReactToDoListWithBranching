4d87da3179b1e387674e9e8f7bd02f9ded80b9fa
MatissBrunenieks <matissbrunenaiks@gmail.com>
2026-09-14 14:41:20 +0300

    fixed app.tsx/fixing main

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
+interface Todo {
+  id: string;
+  text: string;
+  completed: boolean;
+}
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
-  function addTodo(e) {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [input, setInput] = useState<string>('');
+  function addTodo(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     if (!input.trim()) return;
     setTodos([...todos, { id: crypto.randomUUID(), text: input, completed: false }])
     setInput('')
   }
-  function deleteTodo(id) {
+  function deleteTodo(id: string) {
     setTodos(todos.filter(todo => todo.id !== id));
   }
   return (
